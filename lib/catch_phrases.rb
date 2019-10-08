e39c7fcfee13997668556728e5fd93b6264dc106
Scott Matsuda <smatsuda2014@gmail.com>
2019-10-08 18:53:01 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -7,3 +7,12 @@ def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+
+def link
+  phrase = "It's Dangerous To Go Alone! Take this."
+  puts phrase
+end
+
+def any_phrase(phrase = "Do A Barrel Roll!")
+  puts phrase
+end
